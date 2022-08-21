bde75db882b736a7acaae1cd7d9d0fcf5699e347
rainwangpupil <rain-19121186325@outlook.com>
2022-08-21 16:40:57 +0800

    Publish version 1.0.0

---
+++ upload_pkg.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+"""
+Author: Wdataorg
+Date: .08.21
+Project Name: Wdata
+"""
+from os import system
+
+system(r'C:\Users\S.X.Y\AppData\Local\Programs\Python\Python310\python.exe  -m  twine upload  -u __token__ -p {} dist/*'.format(passwd))
+
+folder_list = ['build', 'dist', ' Wdatabase.egg-info']
+
+for i in folder_list:
+    system('rmdir -f {}'.format(i))
